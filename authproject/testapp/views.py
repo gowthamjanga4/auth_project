c21710a40ddc4e619e5ab5916fab37b95750f7af
gowtham <gowthamjanga7@gmail.com>
2024-10-06 17:47:13 +0530

    First commit

---
+++ authproject/testapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm
+from django.http import HttpResponseRedirect
+
+
+# Create your views here.
+
+def home_view(request):
+    return render(request, 'testapp/home.html')
+
+
+@login_required
+def javaexams_view(request):
+    return render(request, 'testapp/javaexams.html')
+
+
+@login_required
+def pythonexams_view(request):
+    return render(request, 'testapp/pythonexams.html')
+
+
+@login_required
+def aptitudeexams_view(request):
+    return render(request, 'testapp/aptitudeexams.html')
+
+
+def logout_view(request):
+    return render(request, 'testapp/logout.html')
+
+
+def signup_view(request):
+    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        user = form.save()
+        user.set_password(user.password)
+        user.save()
+        return HttpResponseRedirect('/accounts/login')
+    return render(request, 'testapp/signup.html', {'form': form})
